b08152900ae6f5e2790ea7a0fe62a40a45606990
zeileis <zeileis@82024d28-4ff0-0310-8bd4-ba695345edfc>
2008-01-16 16:15:57 +0000

    streamlined estfun.hurdle
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sandwich@103 82024d28-4ff0-0310-8bd4-ba695345edfc

--- sandwich/R/estfun.R
+++ sandwich/R/estfun.R
@@ -81,3 +81,68 @@ estfun.nls <- function(x, ...)
   colnames(rval) <- names(coef(x))
   rval
 }
+
+estfun.hurdle <- function(x, ...) {
+  ## extract data
+  Y <- if(is.null(x$y)) model.response(model.frame(x)) else x$y
+  X <- model.matrix(x, model = "count")
+  Z <- model.matrix(x, model = "zero")
+  beta <- coef(x, model = "count")
+  gamma <- coef(x, model = "zero")
+  fulltheta <- x$theta
+
+  offset <- x$offset
+  if(is.null(offset)) offset <- rep(0, NROW(X))
+  if(x$dist$zero == "binomial") li <- make.link(x$link)
+  wts <- weights(x)
+  Y1 <- Y > 0
+
+  ## count component: working residuals
+  eta <- as.vector(X %*% beta + offset)
+  mu <- exp(eta)
+  theta <- fulltheta["count"]
+
+  wres_count <- as.numeric(Y > 0) * switch(x$dist$count,
+    "poisson" = {
+      (Y - mu) - exp(ppois(0, lambda = mu, log.p = TRUE) -
+        ppois(0, lambda = mu, lower.tail = FALSE, log.p = TRUE) + eta)    
+    },
+    "geometric" = {
+      (Y - mu * (Y + 1)/(mu + 1)) - exp(pnbinom(0, mu = mu, size = 1, log.p = TRUE) -
+        pnbinom(0, mu = mu, size = 1, lower.tail = FALSE, log.p = TRUE) - log(mu + 1) + eta)
+    },
+    "negbin" = {
+      (Y - mu * (Y + theta)/(mu + theta)) - exp(pnbinom(0, mu = mu, size = theta, log.p = TRUE) -
+        pnbinom(0, mu = mu, size = theta, lower.tail = FALSE, log.p = TRUE) +
+	log(theta) - log(mu + theta) + eta)
+    })
+  
+  ## zero component: working residuals
+  eta <- as.vector(Z %*% gamma)
+  mu <- if(x$dist$zero == "binomial") li$linkinv(eta) else exp(eta)
+  theta <- fulltheta["zero"]
+
+  wres_zero <- switch(x$dist$zero,
+    "poisson" = {
+      ifelse(Y1, exp(ppois(0, lambda = mu, log.p = TRUE) -
+        ppois(0, lambda = mu, lower.tail = FALSE, log.p = TRUE) + eta), -mu)
+    },
+    "geometric" = {
+      ifelse(Y1, exp(pnbinom(0, mu = mu, size = 1, log.p = TRUE) -
+        pnbinom(0, mu = mu, size = 1, lower.tail = FALSE, log.p = TRUE) - log(mu + 1) + eta), -mu/(mu + 1))
+    },
+    "negbin" = {
+      ifelse(Y1, exp(pnbinom(0, mu = mu, size = theta, log.p = TRUE) -
+        pnbinom(0, mu = mu, size = theta, lower.tail = FALSE, log.p = TRUE) +
+        log(theta) - log(mu + theta) + eta), -mu * theta/(mu + theta))
+    },
+    "binomial" = {
+      ifelse(Y1, 1/mu, -1/(1-mu)) * li$mu.eta(eta)
+    })
+
+  ## compute gradient from data
+  rval <- cbind(wres_count * wts * X, wres_zero * wts * Z)
+  colnames(rval) <- names(coef(x))
+  rownames(rval) <- rownames(X)
+  return(rval)
+}
