a6f6bbb145f43a06ba265b86741723ddf485f0e1
azeileis <azeileis@82024d28-4ff0-0310-8bd4-ba695345edfc>
2017-07-18 23:17:49 +0000

    fully export vcovBS()
    - add documentation
    
    - make vcovBS() generic so that dedicated methods can be added
    
    - the default method gained an argument start=FALSE so that starting
      values can be used in the resampling, typically saving some time
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sandwich@333 82024d28-4ff0-0310-8bd4-ba695345edfc

--- pkg/R/vcovBS.R
+++ pkg/R/vcovBS.R
@@ -1,6 +1,10 @@
 .vcovBSenv <- new.env()
 
-vcovBS <- function(x, cluster = NULL, R = 250, ...)
+vcovBS <- function(x, ...) {
+  UseMethod("vcovBS")
+}
+
+vcovBS.default <- function(x, cluster = NULL, R = 250, start = FALSE, ...)
 {
   ## cluster variable (default 1:n)
   if(is.null(cluster)) {
@@ -12,19 +16,28 @@ vcovBS <- function(x, cluster = NULL, R = 250, ...)
   ## set up coefficient matrix
   cf <- coef(x)
   cf <- matrix(rep.int(0, length(cf) * R), ncol = length(cf),
-               dimnames = list(NULL, names(cf)))
+    dimnames = list(NULL, names(cf)))
+
+  ## use starting values?
+  assign(".vcovBSstart", if(isTRUE(start)) coef(x) else NULL, envir = .vcovBSenv)
 
   ## update on bootstrap samples
   for(i in 1:R) {
       subset <- unlist(cl[sample(names(cl), length(cl), replace = TRUE)])
       assign(".vcovBSsubset", subset, envir = .vcovBSenv)
-      up <- update(x, subset = .vcovBSenv$.vcovBSsubset, evaluate = FALSE)
+      up <- if(is.null(.vcovBSenv$.vcovBSstart)) {
+        update(x, subset = .vcovBSenv$.vcovBSsubset, ..., evaluate = FALSE)
+      } else {
+        update(x, subset = .vcovBSenv$.vcovBSsubset, start = .vcovBSenv$.vcovBSstart, ..., evaluate = FALSE)      
+      }
       env <- try(environment(terms(x)))
       if(inherits(env, "try-error")) env <- NULL
       up <- eval(up, envir = env, enclos = parent.frame())
       remove(".vcovBSsubset", envir = .vcovBSenv)
       cf[i, ] <- coef(up)
   }
+  remove(".vcovBSstart", envir = .vcovBSenv)
+  
   # covariance of bootstrap coefficients
   cov(cf)
 }
