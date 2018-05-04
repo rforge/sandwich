61721d9ff3756f992258e73a0268a864b01a6f05
susanneberger <susanneberger@82024d28-4ff0-0310-8bd4-ba695345edfc>
2018-05-04 13:45:26 +0000

    comparisons with Stata's xtscc and R's vcovSCC added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sandwich@435 82024d28-4ff0-0310-8bd4-ba695345edfc

--- pkg/tests/vcovPL.R
+++ pkg/tests/vcovPL.R
@@ -12,3 +12,20 @@ vcovPL(n, cluster = InstInnovation$industry, adjust = TRUE)
 vcovPL(n, cluster = InstInnovation$industry, adjust = FALSE)
 
 
+# vcovPL covariance matrix compared with vcovSCC from plm package
+pm <- plm::plm(y ~ x, data = PetersenCL, model = "pooling", indexes = c("firm", "year"))
+
+(pl1 <- vcovPL(m, cluster = PetersenCL$firm, adjust = FALSE))
+(pl2 <- plm::vcovSCC(pm))
+attr(pl2, "cluster") <- NULL
+
+all.equal(pl1, pl2, tolerance = 1e-5)
+
+
+# vcovPL compared with Stata's xtscc (xtscc y x, lag(1) ase) standard errors
+pl4 <- c(0.0243573, 0.0281633)
+names(pl4) <- c("(Intercept)", "x")
+pl4
+(pl3 <- sqrt(diag(pl1)))
+
+all.equal(pl3, pl4, tolerance = 1e-5)
