31db02086c492e281535ce43fcb2ef0a69378eed
azeileis <azeileis@82024d28-4ff0-0310-8bd4-ba695345edfc>
2018-08-03 12:31:06 +0000

    some more formatting in script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sandwich@449 82024d28-4ff0-0310-8bd4-ba695345edfc

--- pkg/tests/vcovCL.R
+++ pkg/tests/vcovCL.R
@@ -2,6 +2,7 @@ library("sandwich")
 data("PetersenCL", package = "sandwich")
 m <- lm(y ~ x, data = PetersenCL)
 
+## various versatile variance flavors
 vcovCL(m, cluster = ~ firm, type = "HC0", cadjust = TRUE)
 vcovCL(m, cluster = ~ firm, type = "HC0", cadjust = FALSE)
 vcovCL(m, cluster = ~ firm, type = "HC1", cadjust = TRUE)
@@ -21,7 +22,7 @@ vcovCL(m, cluster = ~ firm + year, type = "HC3", cadjust = TRUE)
 vcovCL(m, cluster = ~ firm + year, type = "HC3", cadjust = FALSE)
 
 
-# vcovCL compared with cluster.vcov from multiwayvcov package
+## comparison with multiwayvcov::cluster.vcov
 
 (cl1 <- vcovCL(m, cluster = ~ firm, type = "HC1", cadjust = TRUE))
 (cl2 <- multiwayvcov::cluster.vcov(m, ~ firm))
@@ -41,16 +42,23 @@ all.equal(cl5, cl6)
 all.equal(cl7, cl8)
 
 
-# vcovCL compared with BMlmSE (Bell-McCaffrey standard errors as described in Imbens and Kolesar 2016,
-# https://github.com/kolesarm/Robust-Small-Sample-Standard-Errors)
-# BMlmSE(m, clustervar = factor(PetersenCL$firm))$vcov
+## comparison with BMlmSE snippet (https://github.com/kolesarm/Robust-Small-Sample-Standard-Errors,
+## Bell-McCaffrey standard errors as described in Imbens and Kolesar 2016)
 
+## BMlmSE(m, clustervar = factor(PetersenCL$firm))$vcov
 bellmc1 <- matrix(c(0.0044944872570532, -6.59291186924326e-05, -6.59291186924326e-05, 0.00256823604178553), nrow = 2)
 rownames(bellmc1) <- colnames(bellmc1) <- c("(Intercept)", "x")
-bellmc1
+
 (bellmc2 <- vcovCL(m, cluster = ~ firm, type = "HC2"))
 all.equal(bellmc1, bellmc2)
 
+
+## comparison with Stata
+
+## FIXME
+
+
+## more examples
 
 data("InstInnovation", package = "sandwich")
 n <- glm(cites ~ institutions, family = "poisson", data = InstInnovation)
